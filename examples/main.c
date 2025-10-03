c454b33e6f9620fe3144408122e6fd2eb72c124b
Oliver Pickford <195665471+opickford@users.noreply.github.com>
2025-10-03 16:55:03 +0100

    implement vec push back

--- examples/main.c
+++ examples/main.c
@@ -1,7 +1,24 @@
 ﻿#include <chds/vector.h>
 
+#include <stdio.h>
+
 int main()
 {
-    
+    Vector(int) v = 0;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        Vector_push_back(v, i);
+    }
+
+    for (int i = 0; i < 100; ++i)
+    {
+        printf("%d) %d\n", i, v[i]);
+    }
+
+    printf("Size: %zu\n", Vector_size(v));
+
+    Vector_destroy(v);
+
     return 0;
 }
