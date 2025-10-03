3e55f3430f3395a5ce233dc53a7b005f379242ff
Oliver <195665471+opickford@users.noreply.github.com>
2025-10-03 18:02:59 +0100

    implement vector reserve (#2)
    * refactor capacity growth into func
    
    * move private funcs to bottom
    
    * document Vector_destroy
    
    * implement reserve func
    
    * add reserve to vector example

--- include/chds/vector.h
+++ include/chds/vector.h
@@ -16,7 +16,7 @@ typedef struct
 // Define a vector type.
 #define Vector(T) T*
 
-// TODO: Private.
+// TODO: Make private.
 // Return the vector header.
 #define CHDS_Vector_header(v) ((CHDS_VectorHeader*)(v) - 1)
 
@@ -28,14 +28,62 @@ typedef struct
 { \
     CHDS_Vector_grow_if_needed(&(v), sizeof(*v)); \
     (v)[CHDS_Vector_header((v))->count++] = (value); \
-    \
- \
+\
 } while (0)
+
+// Increases the capacity of the vector to hold the given number of elements.
+#define Vector_reserve(v, capacity) CHDS_Vector_reserve(&(v), capacity, sizeof(*v))
+
+// Releases the underlying vector array.
+inline void Vector_destroy(void* v)
+{
+    // TODO: Free elements individually? Would require dtor func.
+    if (v) free(CHDS_Vector_header(v));
+}
+
+// TODO: Private methods. Could be hidden by actually turning into lib rather than header only.
+inline CHDS_VectorHeader* CHDS_Vector_set_capacity(CHDS_VectorHeader* h, size_t capacity, size_t element_size)
+{
+    // Copy old header data.
+    size_t count = 0;
+    if (h)
+    {
+        count = h->count;
+    }
+
+    size_t size = sizeof(CHDS_VectorHeader) + capacity * element_size;
+
+    CHDS_VectorHeader* vector = realloc(h, size);
+    if (!vector)
+    {
+        // TODO: How can we possibly handle failure?
+        //       Don't want to leak old h...
+        return 0;
+    }
+
+    // Set output header.
+    vector->capacity = capacity;
+    vector->count = count;
+
+    return vector;
+}
+
+inline void CHDS_Vector_reserve(void** v, size_t capacity, size_t element_size)
+{
+    CHDS_VectorHeader* h = 0;
+    if (*v)
+    {
+        h = CHDS_Vector_header(*v);
+    }
+
+    h = CHDS_Vector_set_capacity(h, capacity, element_size);
+
+    *v = h + 1;
+}
 
 inline void CHDS_Vector_grow_if_needed(void** v, size_t element_size)
 {
     CHDS_VectorHeader* h = 0;
-    size_t count = 0;
     size_t capacity = 1;
 
     if (*v)
@@ -48,31 +96,12 @@ inline void CHDS_Vector_grow_if_needed(void** v, size_t element_size)
         if (extra == 0) extra = 1;
 
         capacity = h->capacity + extra;
-        count = h->count;
     }
     
-    size_t size = sizeof(CHDS_VectorHeader) + capacity * element_size;
-
-    // TODO: Rename from temp.
-    CHDS_VectorHeader* temp = realloc(h, size);
-    if (!temp)
-    {
-        // TODO: How can we possibly handle failure?
-        return;
-    }
-
-    CHDS_VectorHeader* new_header = temp;
-    new_header->capacity = capacity;
-    new_header->count = count;
-
-    *v = new_header + 1;
+    h = CHDS_Vector_set_capacity(h, capacity, element_size);
+    
+    // Set the input array to the new ptr.
+    *v = h + 1;
 }
-
-inline void Vector_destroy(void* v)
-{
-    // TODO: Free elements individually? Would require dtor func.
-    if (v) free(CHDS_Vector_header(v));
-}
-
 
 #endif
