ff180194c2d00da5da86c08c326ad19a71b77968
Oliver Pickford <195665471+opickford@users.noreply.github.com>
2025-10-06 20:53:28 +0100

    add tests for Vector_resize

--- tests/test_vector.c
+++ tests/test_vector.c
@@ -46,13 +46,38 @@ static void test_reserve()
         assert(h->capacity == CAPACITY);
     }
 
-    // Attempt to reserve less elements, this should not
-    // shrink the vector.
+    // Attempt to reserve less elements, this should not shrink the vector.
     Vector_reserve(v, 3);
     {
         CHDS_VectorHeader* h = CHDS_Vector_header(v);
         assert(h);
         assert(h->capacity == CAPACITY);
+    }
+
+    assert(Vector_size(v) == 0);
+
+    Vector_destroy(v);
+}
+
+static void test_resize()
+{
+    const int CAPACITY = 5;
+
+    Vector(int) v = 0;
+    Vector_resize(v, CAPACITY);
+
+    {
+        CHDS_VectorHeader* h = CHDS_Vector_header(v);
+        assert(h);
+        assert(h->capacity == CAPACITY);
+    }
+
+    // Resize to less than current capacity, this should shrink the vector.
+    Vector_resize(v, 3);
+    {
+        CHDS_VectorHeader* h = CHDS_Vector_header(v);
+        assert(h);
+        assert(h->capacity == 3);
     }
 
     assert(Vector_size(v) == 0);
@@ -81,5 +106,6 @@ void test_vector()
     // TODO: test_capacity()?
     test_push_back();
     test_reserve();
+    test_resize();
     test_clear();
 }
