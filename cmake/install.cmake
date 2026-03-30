f8917605f75d4e96c26bf6ba6a1a7f0238be9ac2
Oliver <195665471+opickford@users.noreply.github.com>
2026-03-30 14:47:27 +0100

    build: refactor install process (#20)

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,27 +1,44 @@
-# Export targets
-install(TARGETS chds EXPORT chdsTargets
-    ARCHIVE DESTINATION lib
-    LIBRARY DESTINATION lib
-    RUNTIME DESTINATION bin
-    INCLUDES DESTINATION include
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+# Register chds target for installation.
+install(TARGETS chds
+    EXPORT chdsTargets
+    INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
 
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/include/ DESTINATION include)
+# Copy contents of include dir.
+install(DIRECTORY include/
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+)
 
+# version.h lives in the build dir, so must be installed separately to the other headers.
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/include/chds/version.h
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/chds
+)
+
+# Generate and install chdsTargets.cmake.
 install(EXPORT chdsTargets
     FILE chdsTargets.cmake
     NAMESPACE chds::
-    DESTINATION lib/cmake/chds
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/chds
 )
 
-include(CMakePackageConfigHelpers)
+# Generate chdsConfig for find_package.
 configure_package_config_file(
-    "${CMAKE_SOURCE_DIR}/cmake/chdsConfig.cmake.in"
-    "${CMAKE_BINARY_DIR}/chdsConfig.cmake"
-    INSTALL_DESTINATION lib/cmake/chds
+    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/chdsConfig.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/chdsConfig.cmake"
+    INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/chds"
+)
+
+write_basic_package_version_file(
+    ${CMAKE_CURRENT_BINARY_DIR}/chdsConfigVersion.cmake
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY SameMajorVersion
 )
 
 install(FILES
-    "${CMAKE_BINARY_DIR}/chdsConfig.cmake"
-    DESTINATION lib/cmake/chds
+    ${CMAKE_CURRENT_BINARY_DIR}/chdsConfig.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/chdsConfigVersion.cmake
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/chds
 )
